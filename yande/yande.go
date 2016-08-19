b3884e6d135f290c6689198e190149b027eb822d
Administrator <245561237@qq.com>
2016-08-19 14:46:10 +0800

    获取标签

--- yande/yande.go
+++ yande/yande.go
@@ -16,14 +16,29 @@ type Querier struct {
 	Limit int
 }
 
+// 文章集
 type Result struct {
 	Article []Post `xml:"post"`
 }
+
+// 文章
 type Post struct {
 	Title       string `xml:"id,attr"`
 	Description string `xml:"tags,attr"`
 	PicURL      string `xml:"preview_url,attr"`
 	URL         string `xml:"sample_url,attr"`
+}
+
+// 标签集
+type Tags struct {
+	Tags []Tag `xml:"tag"`
+}
+
+// 标签
+type Tag struct {
+	Id    string `xml:"id,attr"`
+	Name  string `xml:"name,attr"`
+	Count string `xml:"count,attr"`
 }
 
 // 获取目标数据
@@ -45,6 +60,27 @@ func GetPost(que Querier) Result {
 	}
 
 	return result
+}
+
+// 获取目标分类标签
+func GetTags() Tags {
+	// 获取 100 个热门标签
+	resp, err := http.Get("https://yande.re/tag.xml?order=count&limit=100")
+	if err != nil {
+
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+
+	}
+	var tags Tags
+	err = xml.Unmarshal(body, &tags)
+	if err != nil {
+
+	}
+
+	return tags
 }
 
 // 获取资源地址
--- yande/yande_test.go
+++ yande/yande_test.go
@@ -7,6 +7,7 @@ package yande_test
 import (
 	"fmt"
 	//	"log"
+	"bytes"
 	"testing"
 
 	"github.com/yizenghui/gather/yande"
@@ -19,11 +20,26 @@ func Test_NewQuerier(t *testing.T) {
 	fmt.Println(que)
 }
 
-func Test_GetPost(t *testing.T) {
-	que := yande.Querier{"", 1, 10}
-	//	que := yande.NewQuerier("sex", "1", "10")
-	fmt.Println(que)
-	data := yande.GetPost(que)
-	fmt.Println(data)
+//func Test_GetPost(t *testing.T) {
+// que := yande.Querier{"", 1, 10}
+//	que := yande.NewQuerier("sex", "1", "10")
+// fmt.Println(que)
+// data := yande.GetPost(que)
+//	fmt.Println(data)
+
+//}
+
+func Test_GetTags(t *testing.T) {
+	var buffer bytes.Buffer
+	//data := yande.GetTags()
+	//fmt.Println(data)
+	tags := yande.GetTags()
+	for _, v := range tags.Tags {
+		buffer.WriteString(v.Name)
+		buffer.WriteString(" : ")
+		buffer.WriteString(v.Count)
+		buffer.WriteString("\n")
+	}
+	fmt.Println(buffer.String())
 
 }
